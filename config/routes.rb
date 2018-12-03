6807390aea7c9392427e678e09fece6497ef887d
Flatiron School <damanybailey@icloud.com>
2018-12-03 13:36:47 +0000

    created Sin set-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'sins/new'
-  get 'sins/create'
-  get 'sins/edit'
-  get 'sins/update'
-  get 'sins/destroy'
-  get 'sins/index'
-  get 'sins/show'
+ resources :sins
+
+ root "sins#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
