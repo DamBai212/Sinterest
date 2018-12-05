915881db9e3164306e81f13d3419aea40b7f980d
Chris <cwoolnoughchamorro@gmail.com>
2018-12-05 17:08:41 +0000

    added login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :sins
     has_many :categories, through: :sins
+
+    has_secure_password
+    validates :name, presence: true
+    validates :name, uniqueness: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_093305) do
+ActiveRecord::Schema.define(version: 2018_12_05_141315) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2018_12_04_093305) do
     t.text "description"
     t.integer "user_id"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
